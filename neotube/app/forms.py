50ed338d2d4c8a6a837719d116ea3463ec83ea42
김승재 <sjk@gimseungjaeui-MacBookPro.local>
2019-11-18 10:51:02 +0900

    Add gitignore

--- neotube/app/forms.py
+++ neotube/app/forms.py
@@ -7,11 +7,16 @@ class SearchForm (forms.Form):
     word = forms.CharField(label='Search Word')
 
 class VideoForm(forms.ModelForm):
+    """
     name = forms.CharField(max_length=200, widget=forms.TextInput(
         attrs={'class':"form-control", 'id':"name", 'placeholder':"영상 제목 입력"}))
     videofile = forms.FileField(upload_to='media/videos/%Y/%m/%d/', null=True, verbose_name="")
     slug = forms.SlugField(unique=True)
     timestamp   = forms.DateTimeField(auto_now_add=True)
+    """
+    name = forms.CharField(max_length=200, widget=forms.TextInput(
+        attrs={'class':"form-control", 'id':"name", 'placeholder':"영상 제목 입력"}))
+    videofile = forms.FileField()
     class Meta:
         model= Video
         fields= ["name", "videofile"]
